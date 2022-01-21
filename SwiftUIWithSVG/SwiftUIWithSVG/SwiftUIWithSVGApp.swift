43a820543ce217c1e4e79a931cd1c4982e11a209
jyang <jTang@walkgame.com>
2022-01-21 12:31:25 +0800

    init project

---
+++ SwiftUIWithSVG/SwiftUIWithSVG/SwiftUIWithSVGApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithSVGApp.swift
+//  SwiftUIWithSVG
+//
+//  Created by Apple on 2022/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithSVGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
